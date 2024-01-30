3c214430bde2d23090601ec00eaf034dbdafe420
xy <xyzmhx@gmail.com>
2024-01-30 15:44:08 +0900

    Update `nostr_wrapper` to read relays from config file
    Added serde and serde_yaml dependencies to `nostr_wrapper` and implemented a config struct to read relay settings from a YAML file. Replaced hardcoded relay array in `publish_text_note` function with relay list from config object. Also, version of indexmap dependency was updated in `Cargo.lock`.

--- nostr_wrapper/src/lib.rs
+++ nostr_wrapper/src/lib.rs
@@ -1,1 +1,12 @@
 pub mod publish;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub nostr: Nostr,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Nostr {
+    pub relays: Vec<String>,
+}
--- nostr_wrapper/src/publish.rs
+++ nostr_wrapper/src/publish.rs
@@ -1,6 +1,14 @@
 use nostr_sdk::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+use serde_yaml::Error;
+use crate::Config;
+
 
 pub async fn publish_text_note(my_keys:Keys,username: &str, message: &str) -> Result<()> {
+    let file = File::open("./conf/test/config.yaml").expect("Failed to open config file");
+    let reader = BufReader::new(file);
+    let config: Config = serde_yaml::from_reader(reader).expect("Failed to read config");
     // show keys
     let bech32_pubkey: String = my_keys.public_key().to_bech32()?;
     println!("Bech32 PubKey: {}", bech32_pubkey);
@@ -9,17 +17,7 @@ pub async fn publish_text_note(my_keys:Keys,username: &str, message: &str) -> Re
     let client = Client::new(&my_keys);
 
     // add relays
-    let relays = vec![
-        "wss://relay.damus.io",
-        "wss://freerelay.xyz",
-        "wss://relayable.org",
-        "wss://nos.lol",
-        "wss://offchain.pub",
-        "wss://relay.snort.social",
-        "wss://bitcoiner.social",
-        "wss://relay.current.fyi",
-    ];
-    client.add_relays(relays).await?;
+    client.add_relays(config.nostr.relays).await?;
 
     // connect to the network
     client.connect().await;
