58081a2b26e905a00a740accca59d5cf8bcdabb1
xy <xyzmhx@gmail.com>
2024-04-12 14:45:20 +0900

    Update run_migrations to use external config loader
    The main function in run_migrations.rs was updated to use an external configuration loader. The Database Source Name (DSN) is now retrieved from the configuration file using the loader, in place of the hard-coded string, which enhances modularity and reusability of code. A constant string for the configuration path was also added.

--- src/bin/run_migrations.rs
+++ src/bin/run_migrations.rs
@@ -1,10 +1,15 @@
 use data::DbConnection;
+use youtube_bot::load_conf;
+
+const CONF_PATH: &str = "./conf/test/config.yaml";
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let dsn = "./conf/test/config.yaml";
-    let mut db_conn = match DbConnection::new(dsn) {
+    let conf = load_conf(CONF_PATH).expect("Failed to load config");
+
+    let dsn = conf.postgres.dsn;
+    let mut db_conn = match DbConnection::new(&dsn) {
         Ok(conn) => conn,
         Err(e) => {
             log::error!("Failed to create database connection: {}", e);
